1a3d84e14af5af2ee0fa5da5df1f52b65dfce2f4
Tebelelo <tebelekoana@gmail.com>
2025-12-12 15:18:18 +0200

    task api

--- project-management-app/src/pages/Tasks.jsx
+++ project-management-app/src/pages/Tasks.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 // CHANGE THIS TO MATCH YOUR BACKEND
-const API_BASE = 'http://localhost:4001/api';
+const API_BASE = "https://collabtrack-oruu.onrender.com/api";
 
 export default function Tasks() {
   const navigate = useNavigate();
